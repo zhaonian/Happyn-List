6d499b26a2bf63293b4d83a7a5c14afef4d0c070
Zhaonian Luan <zhaonial@uci.edu>
2017-03-05 22:26:34 -0800

    finished log in

---
+++ app/src/main/java/com/searchsystem/mycommunity/EventsActivity.java
@@ -0,0 +1,16 @@
+package com.searchsystem.mycommunity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Luan on 3/5/17.
+ */
+
+public class EventsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events);
+    }
+}
